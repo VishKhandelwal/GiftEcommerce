59b9d8454e0e0cc6a87538758f22d75d0bfd9de6
Vish Khandelwal <vaishali.kh2310@gmail.com>
2025-06-15 17:27:47 +0530

    Remove pywin32 and add runtime.txt for Render deployment

--- Gift_ecommerce/settings.py
+++ Gift_ecommerce/settings.py
@@ -159,6 +159,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
