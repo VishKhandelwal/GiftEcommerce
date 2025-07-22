5c35f0350fc2cc67585711e80ecb3a88e3a1a289
Vish Khandelwal <vaishali.kh2310@gmail.com>
2025-07-22 20:03:39 +0530

    Update all changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     # urls.py
-    path('', views.login_register, name='login_register'),
     path('login/', views.login_view, name='login'),
     path('new_user/', views.new_user, name='new_user'),
     path('verify/', views.verify_otp, name='verify'),
