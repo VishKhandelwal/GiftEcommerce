bca5fb37d4e90195918d796460310b4330c3f466
Vish Khandelwal <vaishali.kh2310@gmail.com>
2025-07-26 17:52:21 +0530

    Update all changes

--- Gift_ecommerce/settings.py
+++ Gift_ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%e7g3_o!j629t5)aim2inxg(7zh9rbqy9n1*7&z=vna@pda*a+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['infinityforbusiness.com', 'www.infinityforbusiness.com', '.onrender.com']
 SECURE_SSL_REDIRECT = True  # okay to keep, but avoid forcing host redirects
