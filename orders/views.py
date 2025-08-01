61d895f2beb20ed3ab0d8062de8cf8d98ff352e9
Vish Khandelwal <vaishali.kh2310@gmail.com>
2025-08-01 16:16:30 +0530

    Update all changes

--- orders/views.py
+++ orders/views.py
@@ -18,8 +18,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
-
-@login_required(login_url='accounts:login')
 def order_summary_view(request):
     latest_order = Order.objects.filter(user=request.user).last()
     return render(request, 'orders/summary.html', {
